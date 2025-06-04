d998e2b20d101eb11f192087fe0bb2110ab05b61
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-06-04 11:43:29 +0900

    App修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [text, setText] = useState('');
 
-  const handleAdd = () => {
+  const addTodo = (text) => {
     setTodos([...todos, { id: Date.now(), text }]);
-    setText('');
   };
 
   return (
     <div>
       <h1>TODO</h1>
-      <input value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={handleAdd}>追加</button>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
+      <TodoForm onAdd={addTodo} />
+      <TodoList todos={todos} />
     </div>
   );
 }
