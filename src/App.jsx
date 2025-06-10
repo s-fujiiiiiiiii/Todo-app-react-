a75dd5d55cca7783700293de3b89007184f4b54b
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-06-10 12:11:10 +0900

    useEffect追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem('my-todos');
+    if (saved) {
+      setTodos(JSON.parse(saved));
+    }
+  }, []);
+
+  useEffect (() => {
+    localStorage.setItem('my-dotos', JSON.stringify(todos));
+  }, [todos]);
   
   const addTodo = (text) => {
     setTodos([...todos, { id:Date.now(), text, done: false }]);
