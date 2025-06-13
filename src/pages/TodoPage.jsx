d27118899446297beffe82982ebfe1cfcfe5020f
fujiiiiiiiii <fko2447019@stu.o-hara.ac.jp>
2025-06-13 10:24:44 +0900

    フィルター機能の追加

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,13 +1,30 @@
-import TodoForm from "../components/TodoForm";
-import TodoList from "../components/TodoList";
+import { useState } from 'react';
+import TodoForm from '../components/TodoForm';
+import TodoList from '../components/TodoList';
 
 function TodoPage({ todos, addTodo, toggleTodo, deleteTodo }) {
-    return (
-        <div>
-            <TodoForm onAdd={addTodo} />
-            <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
-        </div>
-    )
+	const [filter, setFilter] = useState('all');
+	
+	const filteredTodos = todos.filter(todo => {
+	  if (filter === 'done') return todo.done;
+	  if (filter === 'notYet') return !todo.done;
+	  return true;
+	});
+
+  return (
+    <div>
+      <TodoForm onAdd={addTodo} />
+      
+      {/* フィルターボタン */}
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={() => setFilter('all')}>すべて</button>
+        <button onClick={() => setFilter('notYet')}>未完了</button>
+        <button onClick={() => setFilter('done')}>完了</button>
+      </div>
+            
+      <TodoList todos={filteredTodos} onToggle={toggleTodo} onDelete={deleteTodo} />
+    </div>
+  );
 }
 
 export default TodoPage;
